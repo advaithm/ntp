39780844f334818ae980a9eb27bf54986ae2440e
EnderDev <kieran@dothq.co>
2020-12-03 20:45:05 +0000

    ♻️ Start rewrite with NextJS

--- src/widgets/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type WidgetName = string;
-export type WidgetId = string;
-export type WidgetAuthor = string;
-
-export type WidgetPosition = [string | 'center' | 'top' | 'left' | 'right' | 'bottom', string | 'center' | 'top' | 'left' | 'right' | 'bottom']
-export type WidgetScale = number;

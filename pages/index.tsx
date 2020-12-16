dfc45a59aa2bfb195608a25221d4f3c435845554
Andrew <bigspeedfpv@gmail.com>
2020-12-16 17:30:14 -0500

    ⚡️ Cache images to prevent unnecessary API hits

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { NextPageContext } from 'next';
+import { NextPageContext, GetServerSideProps, GetServerSidePropsContext } from 'next';
 import React from 'react';
 import Layout from '../components/layout'
 import axios from 'axios'
@@ -10,24 +10,43 @@ import { log } from '../tools/log';
 import localForage from 'localforage';
 
 const NTPPage = ({ background, attribution }: { background: string; attribution: Attribution }) => {
+  const [backgroundImage, setBackgroundImage] = React.useState("")
+  const [attributionData, setAttributionData] = React.useState<Attribution>()
+
   React.useEffect(() => {
-    if(!background) return;
+    if(!background) {
+      localForage.getItem("wallpaper-cache")
+        .then((cachedWallpaper) => {
+          setBackgroundImage(cachedWallpaper)
+        })
+        .catch((e) => console.log(e))
+      localForage.getItem("attribution-cache")
+        .then((cachedAttribution) => {
+          console.log(cachedAttribution)
+          setAttributionData(cachedAttribution)
+        })
+        .catch((e) => console.log(e))
+      return
+    }
 
     log("CACHE_DRIVER", "Adding background to offline store...");
 
     localForage.setItem("wallpaper-cache", background);
+    localForage.setItem("attribution-cache", attribution)
+    document.cookie = "backgroundSet=true"
+    setBackgroundImage(background)
+    setAttributionData(attribution)
   }, [background])
 
   return (
-    <Layout background={background} attribution={attribution}>
-    </Layout>
+    <Layout background={backgroundImage} attribution={attributionData} />
   )
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  if (ctx.req.cookies.backgroundSet) return { props: { background: "", attribution: {} }};
   const img = await getUnsplash() ?? { background: "", attribution: {} }
   return {
-    revalidate: 60,
     props: {
       background: img.background,
       attribution: img.attribution

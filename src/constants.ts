a814067606e5d3a27bacb4231f031d2af0698df0
EnderDev <kieran@dothq.co>
2020-12-26 19:03:58 +0000

    q

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const PLUGIN_ID_REGEX = /[a-z]+\.[a-z]+\.[a-z]+/;
-export const PLUGIN_NAME_REGEX = /^[a-zA-Z0-9_.-].*$/;
-export const PLUGIN_AUTHOR_REGEX = /([^"]*)?\s*\<([^\s]+@.+)\>/;
--- tools/log.ts
+++
@@ -1,9 +0,0 @@
-const drivers: any = {
-    CACHE_DRIVER: "Cache"
-}
-
-export const log = (driver: any, ...data: any[]) => {
-    if(!drivers[driver]) console.log(`Driver not found: ${driver}`)
-
-    console.log(`[${drivers[driver]}]`, ...data)
-}

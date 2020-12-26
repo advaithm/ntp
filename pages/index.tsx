cbc18fb19eb2e7c6c6ff56023bfd50540fcb9d3e
Andrew <bigspeedfpv@gmail.com>
2020-12-26 13:50:55 -0500

    🐛 Fix getStaticProps revalidate timeout

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
       background: img.background,
       attribution: img.attribution
     },
-    revalidate: 10
+    revalidate: 300
   }
 };
 
